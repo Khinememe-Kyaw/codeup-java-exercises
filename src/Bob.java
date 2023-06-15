4732cd29fecf5e704e71cbbac093ab1730c93720
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-15 13:23:19 -0500

    Use Switch inside if esle instead of if else

--- src/Bob.java
+++ src/Bob.java
@@ -1,28 +1,66 @@
+//import java.util.Scanner;
+//
+//public class Bob {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String BobAnswer;
+//        boolean moreQuestion;
+//
+//        do {
+//            System.out.print("Ask a question to Bob: ");
+//            String input = scanner.nextLine();
+//            if (input.endsWith("?")) {
+//                BobAnswer = "Sure";
+//            } else if (input.endsWith("!")) {
+//                BobAnswer = "Whoa, chill out!";
+//            } else if (input.isEmpty()) {
+//                BobAnswer = "Fine. Be that way!";
+//            } else {
+//                BobAnswer = "Whatever";
+//            }
+//            System.out.println(BobAnswer);
+//            System.out.print("Continue talking to Bob? (y/n): ");
+//            String userInput = scanner.nextLine();
+//            moreQuestion= userInput.equals("y");
+//        }while(moreQuestion);
+//    }
+//}
+
 import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String BobAnswer;
+        String bobAnswer;
         boolean moreQuestion;
 
         do {
             System.out.print("Ask a question to Bob: ");
             String input = scanner.nextLine();
-            if (input.endsWith("?")) {
-                BobAnswer = "Sure";
-            } else if (input.endsWith("!")) {
-                BobAnswer = "Whoa, chill out!";
-            } else if (input.isEmpty()) {
-                BobAnswer = "Fine. Be that way!";
+            System.out.println("input len=" + input.length());
+
+            if (input.length() == 0) {
+                bobAnswer = "Fine. Be that way!";
             } else {
-                BobAnswer = "Whatever";
+                Character lastChar = input.charAt(input.length() - 1);
+                switch (lastChar) {
+                    case '?':
+                        bobAnswer = "Sure";
+                        break;
+                    case '!':
+                        bobAnswer = "Whoa, chill out!";
+                        break;
+                    default:
+                        bobAnswer = "Whatever";
+                }
             }
-            System.out.println(BobAnswer);
+
+
+            System.out.println(bobAnswer);
             System.out.print("Continue talking to Bob? (y/n): ");
             String userInput = scanner.nextLine();
             moreQuestion= userInput.equals("y");
-        }while(moreQuestion);
+        } while (moreQuestion);
     }
 }
 
