eab980172eec738aeb3517e6f0c1b83e5daa6166
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-15 10:24:55 -0500

    Edited version after walkthrough

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -23,44 +23,63 @@ public class ControlFlowExercises {
 //        long i=2;
 //        do{
 //            System.out.println(i);
-//            i= i*i;
+//            i*=i;
 //        }while(i <1000000);
-//        for(int i=5; i<=15; i++){
-//            System.out.println(i);
-//        }
-//        for(int i=0; i<=100; i+=2){
-//            System.out.println(i);
-//        }
-//        for (int i=100; i>= -5;i-=5){
-//            System.out.println(i);
-//        }
-//        for(long i=2; i<1000000; i=i*i){
-//            System.out.println(i);
-//        }
-//        for(int i=0; i<=100; i++){
-//            if(i%3==0 & i%5==0){
-//                System.out.println("FizzBuzz");
-//            }
-//            else if(i!=0 && i%3==0){
-//                System.out.println("Fizz");
-//            }
-//            else if(i!=0 && i%5==0){
-//                System.out.println("Buzz");
-//            }else{
-//                System.out.println(i);
-//            }
-//        }
-//        System.out.print("What number would you like to go up to? ");
-//        int num = scanner.nextInt();
-//        System.out.println("Here is your table!");
-//        System.out.println("number | squared | cubed");
-//        System.out.println("------ | ------- | -----");
-//
-//        for(int i=1; i<= num ;i++){
-//            int squared = i*i;
-//            int cubed = i*i*i;
-//            System.out.printf("%-6d | %-7d | %d%n", i, squared, cubed);
-//        }
+        for(int i=5; i<=15; i++){
+            System.out.println(i);
+        }
+        for(int i=0; i<=100; i+=2){
+            System.out.println(i);
+        }
+        for (int i=100; i>= -5;i-=5){
+            System.out.println(i);
+        }
+        for(long i=2; i<1000000; i=i*i){
+            System.out.println(i);
+        }
+        for(int i=0; i<=100; i++){
+            if(i%3==0 & i%5==0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i!=0 && i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i!=0 && i%5==0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+        System.out.print("What number would you like to go up to? ");
+        int num = scanner.nextInt();
+        System.out.println("Here is your table!");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+
+        for(int i=1; i<= num ;i++){
+            int squared = i*i;
+            int cubed = i*i*i;
+            System.out.printf("%-6d | %-7d | %d%n", i, squared, cubed);
+        }
+
+//        David's Answer
+        boolean WantsToContinue;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("What number would you like to go up to? ");
+            int Num = scanner.nextInt();
+            System.out.println("Here is your table! \n number | squared | cubed \n ------ | ------- | -----");
+            for(int i=1; i<= Num ;i++){
+                System.out.printf("%-6d | %-7d | %d%n", i, i*i, i*i*i);
+            }
+            System.out.println("Would you like to enter another number?(y/n)");
+//            scan.nextLine();
+            String UserAns= scan.nextLine();
+            WantsToContinue= UserAns.equals("y");
+        }while(WantsToContinue);
+
+
         String option;
         do {System.out.print("Type the number between 0 to 100 :  ");
             int Input = scanner.nextInt();
@@ -94,6 +113,7 @@ public class ControlFlowExercises {
             System.out.print("Do you want to continue? (y/n): ");
             option = scanner.next();
         }while(option.equals(("y")));
+
     }
 }
 
