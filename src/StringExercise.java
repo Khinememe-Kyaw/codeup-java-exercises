a35eb026fe9259c8c40f9e677c8ad868555ffabc
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-15 12:12:52 -0500

    Completed String Exercise using escape character

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String Str11 = "We don't need no education \nWe don't need no thought control";
+        System.out.println(Str11 );
+
+        String Str21 ="Check \"this\" out!, \"s inside of \"s! ";
+        System.out.println(Str21);
+
+        String Str31="In windows, the main drive is usually C:\\";
+        System.out.println(Str31);
+
+        String Str41="I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(Str41);
+    }
+}
