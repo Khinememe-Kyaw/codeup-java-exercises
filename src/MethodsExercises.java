591933d5bc92fd86f2ab5feff48a72884a080fab
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-16 10:25:22 -0500

    Added multiplication method using for loop and checked xdivison error when it was divided by 0

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,12 +1,12 @@
 public class MethodsExercises {
     public static void main(String[] args){
-        int num1 = 10;
-        int num2 = 2;
-        Addition(num1,num2);
-        Subtraction(num1, num2);
-        Multiplication(num1, num2);
-        Division(num1, num2);
-        Modulus(num1, num2);
+        Addition(10,8);
+        Subtraction(10,3);
+        Multiplication(3,6);
+        Division(18,3);
+        Modulus(19,2);
+//        System.out.println(division(18,0)); //Exception in thread "main" java.lang.ArithmeticException: / by zero
+        System.out.println(multiplication(2,-3));
     }
     public static void Addition(int num1, int num2){
         System.out.println(num1 + num2);
@@ -22,6 +22,33 @@ public class MethodsExercises {
     }
     public static void Modulus(int num1, int num2){
         System.out.println(num1 % num2);
+    }
+    public static int division(int num1, int num2){
+        return num1/num2;
+    }
+    public static int multiplication (int num1, int num2){
+        int result =0;
+        boolean isNeg =false;
+        if(num1<0 && num2<0){
+            num1= -num1;
+            num2= -num2;
+        }else if(num1<0 || num2<0){
+            isNeg= true;
+            if(num1<0){
+                num1= -num1;
+            }else{
+                num2= -num2;
+            }
+        }
+        for (int i=0; i<num2; i++){
+            result += num1;
+        }
+        if(isNeg=true){
+            result= -result;
+        }else{
+            result= result;
+        }
+        return result;
     }
 }
 
