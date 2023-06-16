6c11dc5bc5ea69cfbadf824d31a1944d41fd0e4e
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-16 10:39:32 -0500

    Commented outr previous 4 arithmetic methods and rewrite using recurrsion

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,27 +1,43 @@
 public class MethodsExercises {
     public static void main(String[] args){
-        Addition(10,8);
-        Subtraction(10,3);
-        Multiplication(3,6);
-        Division(18,3);
-        Modulus(19,2);
+        System.out.println(Addition(10,8));
+        System.out.println(Subtraction(10,3));
+        System.out.println(Multiplication(3,6));
+        System.out.println(Division(18,3));
+        System.out.println(Modulus(19,2));
 //        System.out.println(division(18,0)); //Exception in thread "main" java.lang.ArithmeticException: / by zero
         System.out.println(multiplication(2,-3));
     }
-    public static void Addition(int num1, int num2){
-        System.out.println(num1 + num2);
+//    public static void Addition(int num1, int num2){
+//        System.out.println(num1 + num2);
+//    }
+//    public static void Subtraction(int num1, int num2){
+//        System.out.println(num1 - num2);
+//    }
+//    public static void Multiplication(int num1, int num2){
+//        System.out.println(num1 * num2);
+//    }
+//    public static void Division(int num1, int num2){
+//        System.out.println(num1 / num2);
+//    }
+//    public static void Modulus(int num1, int num2){
+//        System.out.println(num1 % num2);
+//    }
+    public static int Addition(int num1, int num2){
+        return(num1 + num2);
     }
-    public static void Subtraction(int num1, int num2){
-        System.out.println(num1 - num2);
+    public static int Subtraction(int num1, int num2){
+        return(num1 - num2);
     }
-    public static void Multiplication(int num1, int num2){
-        System.out.println(num1 * num2);
+    public static int Multiplication(int num1, int num2){
+        return(num1 * num2);
     }
-    public static void Division(int num1, int num2){
-        System.out.println(num1 / num2);
+    public static int Division(int num1, int num2){
+        return(num1 / num2);
     }
-    public static void Modulus(int num1, int num2){
-        System.out.println(num1 % num2);
+    public static int Modulus(int num1, int num2)
+    {
+        return(num1 % num2);
     }
     public static int division(int num1, int num2){
         return num1/num2;
