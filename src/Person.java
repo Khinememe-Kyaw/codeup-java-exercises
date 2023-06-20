250be122e76af39b87a7ca769e13c62733d487f4
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-20 12:35:19 -0500

    Completed basic object using constructor, void and string methods

---
+++ src/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    private String name;
+//    constructor
+    public Person(String name){
+        this.name= name;
+    }
+    public String getName(){
+        return name;
+//TODO: return the person's name
+    }
+
+    public void setName(String name){
+        System.out.println(name);
+//TODO: change the name field to the passed value
+    }
+    public void sayHello(){
+        System.out.println("Hello "+ name);
+//TODO: print a message to the console using the person's name
+    }
+    public static void main(String[] args){
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName())); //true
+//        System.out.println(person1 == person2); //false
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2); //true
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName()); //John
+//        System.out.println(person2.getName()); //John
+//        person2.setName("Jane"); //Jane
+//        System.out.println(person1.getName()); //John
+//        System.out.println(person2.getName()); //John
+
+        Person person1 = new Person("Joe");
+        Person person2 = new Person("Jack");
+        System.out.println(person1.getName());
+        person1.sayHello();
+        person2.setName("Jack");
+        System.out.println(person2.getName());
+        person2.sayHello();
+    }
+
+}
