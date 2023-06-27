bc3e97cc0090630b8779e1308b3ebff288a9e7e8
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-27 03:12:54 -0500

    Overeide both method in Square class and box 2 is instance of the square class and box 1 is instance of the rectangle class

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,8 +1,19 @@
 package shapes;
 
 public class Square extends Rectangle{
+    double side;
     public Square(double side) {
         super(side, side);
+        this.side = side;
+    }
 
+    @Override
+    public double getArea() {
+        return Math.pow(side,2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4*side;
     }
 }
