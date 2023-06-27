d8aefa0583a0339d035f8e66df1c59cb6e9dd7f8
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-06-27 02:44:08 -0500

    Created box2 with new instance of the Square and passed through both methods

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -14,6 +14,10 @@ public class ShapesTest {
         System.out.println("Box-1 Area : " + box1.getArea());
         System.out.println("Box-1 Perimeter : " +box1.getPerimeter());
 
+        Rectangle box2 = new Square(5);
+        System.out.println("Box-2 Area : " + box2.getArea());
+        System.out.println("Box-2 Perimeter : " +box2.getPerimeter());
+
 
     }
 }
